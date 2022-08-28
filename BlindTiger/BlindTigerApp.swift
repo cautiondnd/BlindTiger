f3bd7724faec9b7da256d7a669c81736726e8ca1
Dante Delgado <dantedelgado111@gmail.com>
2022-08-28 15:53:05 -0500

    final (second submission -- added sign in with apple and tabor background)

--- BlindTiger/BlindTigerApp.swift
+++ BlindTiger/BlindTigerApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import Firebase
 import GoogleSignIn
 import GoogleMobileAds
+import CryptoKit
+import AuthenticationServices
 
 
 @main
@@ -22,7 +24,7 @@ struct BlindTigerApp: App {
           
             let hasSignedIn = UserDefaults.standard.bool(forKey: "hasSignedIn")
             if hasSignedIn == true {
-                TabBarView(selectedTab: 0)
+                TabBarView()
             }
             else {
               //  SchoolSelection(info: self.delegate)
@@ -84,6 +86,33 @@ class AppDelegate: NSObject, UIApplicationDelegate, GIDSignInDelegate, Observabl
         print(error.localizedDescription)
     }
     
+    @Published var nonce = ""
+    
+    func appleAuthinticate(credential: ASAuthorizationAppleIDCredential) {
+        
+        guard let token = credential.identityToken else {
+            print("error w firebase")
+            return
+        }
+        
+        guard let tokenString = String(data: token, encoding: .utf8) else {
+            print("error with Token")
+            return
+        }
+        
+        let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: tokenString, rawNonce: nonce)
+        
+        Auth.auth().signIn(with: firebaseCredential) { (result, err) in
+            if let error = err {
+                print(error.localizedDescription)
+                return
+            }
+            
+            //user logged in
+            self.validateSignIn()
+        }
+    }
+    
 
     
     func validateSignIn() {
@@ -143,7 +172,6 @@ class AppDelegate: NSObject, UIApplicationDelegate, GIDSignInDelegate, Observabl
     
     func fetchSchools() {
         
-        
         db.collection("BlindTiger").whereField("featured", isEqualTo: true).getDocuments { (query, err) in
             
             guard let documents = query?.documents else{return}
@@ -186,24 +214,54 @@ class AppDelegate: NSObject, UIApplicationDelegate, GIDSignInDelegate, Observabl
 
 
 let db = Firestore.firestore()
-//drop everything up to @
-//let email = Auth.auth().currentUser?.email?.drop(while: { (Character) -> Bool in
-//    return Character != "@"
-//})
-//
-//var currentSchool = email?.dropLast(4).drop(while: { (Character) -> Bool in
-//    if (email?.filter({ $0 == "." }).count)! > 1 {
-//        //if theres multiple . then delete everything up to the first one
-//        return Character != "."
-//    }
-//    else{
-//        //if not then delete everything up to the @
-//        return Character != "@"
-//    }
-//}).dropFirst(1)
-//
+
 let seperatedEmail = Auth.auth().currentUser?.email?.components(separatedBy: "@").last!.components(separatedBy: ".")
 let userSchool = seperatedEmail?[(seperatedEmail?.count ?? 0) -  2] ?? "gmail"
 var cleanSchool = UserDefaults.standard.string(forKey: "cleanSchool") ?? userSchool
 //var cleanSchool = "vanderbilt"
 
+// helpers for apple signin
+func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: [Character] =
+    Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError(
+          "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+        )
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+    
+private func sha256(_ input: String) -> String {
+  let inputData = Data(input.utf8)
+  let hashedData = SHA256.hash(data: inputData)
+  let hashString = hashedData.compactMap {
+    String(format: "%02x", $0)
+  }.joined()
+
+  return hashString
+}
